f1f9973095160b98cf9b4fb047e00be2063ee2c7
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-20 16:19:34 -0400

    feat: role errors added to the core package

---
+++ lib/src/role/role_errors.dart
@@ -0,0 +1,8 @@
+class RoleErrorCodes {
+  const RoleErrorCodes._();
+
+  static const String alreadyAssigned = 'user.role.already.assigned';
+  static const String notAssignable = 'user.role.not.assignable';
+  static const String invalid = 'user.role.invalid';
+  static const String assignmentFailed = 'user.role.assignment.failed';
+}
